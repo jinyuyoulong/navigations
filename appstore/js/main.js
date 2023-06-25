ae63f5e33865b36f98e07feb27e8c3c9691bbfac
fan_mac <fan.jinlong@qq.com>
2023-06-26 00:50:51 +0800

    重构navigate 为vue

--- appstore/js/main.js
+++ appstore/js/main.js
@@ -3,72 +3,17 @@ var app = new Vue({
     data: {
         groups:[{
             category: "实用工具",
-            urls: [{
-                    url: "https://www.wps.cn/",
-                    title: "office办公软件 Word Excel PowerPoint",
-                    content: "WPS",
-                    calss: "item"
-                },
-                {
-                    url: "https://pinyin.sogou.com/",
-                    title: "",
-                    content: "搜狗输入法",
-                    calss: "item"
-                },
-                {
-                    url: "https://www.typora.io/",
-                    title: "好用的Markdown编辑器",
-                    content: "Typora",
-                    calss: "item"
-                },
-                {
-                    url: "https://www.google.cn/intl/zh-CN/chrome/",
-                    title: "好用的浏览器",
-                    content: "Chrome谷歌浏览器",
-                    calss: "item"
-                },
-                {
-                    url: "https://www.xunlei.com/",
-                    title: "",
-                    content: "迅雷",
-                    calss: "item"
-                },
-                {
-                    url: "http://pan.baidu.com/download#pan",
-                    title: "",
-                    content: "百度云盘",
-                    calss: "item"
-                },
-                {
-                    url: "https://im.qq.com/",
-                    title: "",
-                    content: "QQ",
-                    calss: "item"
-                },
-                {
-                    url: "https://sparanoid.com/lab/7z/",
-                    title: "解压缩工具",
-                    content: "7-Zip",
-                    calss: "item"
-                },
-                {
-                    url: "http://www.winrar.com.cn/",
-                    title: "rar解压缩",
-                    content: "WinRAR",
-                    calss: "item"
-                },
-                {
-                    url: "https://freemind.en.softonic.com/",
-                    title: "思维导图",
-                    content: "freemind 思维导图",
-                    calss: "item"
-                },
-                {
-                    url: "https://www.microsoft.com/zh-cn/edge/",
-                    title: "",
-                    content: "Microsoft Edge 浏览器",
-                    calss: "item"
-                },
+            urls: [{url: "https://www.wps.cn/", title: "office办公软件 Word Excel PowerPoint", content: "WPS", calss: "item"},
+                {url: "https://pinyin.sogou.com/",   title: "",content: "搜狗输入法",calss: "item"},
+                {url: "https://www.typora.io/",title: "好用的Markdown编辑器",content: "Typora",calss: "item"},
+                {url: "https://www.google.cn/intl/zh-CN/chrome/",title: "好用的浏览器",content: "Chrome谷歌浏览器",calss: "item"},
+                {url: "https://www.xunlei.com/",title: "",content: "迅雷",calss: "item"},
+                {url: "http://pan.baidu.com/download#pan",title: "",content: "百度云盘",calss: "item"},
+                {url: "https://im.qq.com/",title: "",content: "QQ",calss: "item"},
+                {url: "https://sparanoid.com/lab/7z/",title: "解压缩工具",content: "7-Zip",calss: "item"},
+                {url: "http://www.winrar.com.cn/",title: "rar解压缩",content: "WinRAR",calss: "item"},
+                {url: "https://freemind.en.softonic.com/",title: "思维导图",content: "freemind 思维导图",calss: "item"},
+                {url: "https://www.microsoft.com/zh-cn/edge/",title: "",content: "Microsoft Edge 浏览器",calss: "item"},
             ]
         }]
     } 
---
+++ js/main.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        groups: [
+            {
+                category:"论坛",
+                urls:[
+                    {url: "https://www.v2ex.com/",title: "",content:"v2ex", class:"item"},
+                    {url: "https://www.macx.cn/",title: "",content:"苹果福利社", class:"item"}
+                ]
+            }
+        ]
+    }
+})
